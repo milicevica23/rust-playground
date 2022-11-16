a1000b7e6a4e3449bc08cef14b752036e4bfe88e
Aleks <milicoa97@gmail.com>
2022-11-16 20:44:51 +0100

    refactor code for testing; add first unit test; add first integration test

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,8 +1,8 @@
 
+use assert_fs::prelude::FileWriteStr;
 use clap::Parser;
 use std::io::{BufReader, BufRead};
 use std::fs::File;
-mod test;
 
 #[derive(Parser)]
 struct Cli {
@@ -13,19 +13,11 @@ struct Cli {
     file_path: std::path::PathBuf,
 }
 
-fn main() {
-    let args = Cli::parse();
-
-    /*let content = std::fs::read_to_string(&args.file_path).expect("could not read file");
-
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }*/
-
-    let f1 = File::open(&args.file_path);
-    let mut reader = BufReader::new(f1.expect("error"));
+pub fn find_patern_in_file( pattern: &String,
+                            file_path: impl AsRef<std::path::Path>,
+                            mut writer: impl std::io::Write) {
+    let f1 = File::open(file_path).expect("file not found");
+    let mut reader = BufReader::new(f1);
 
     loop {
         let mut line = String::new();
@@ -34,11 +26,34 @@ fn main() {
         match result {
             Ok(0) => break,
             Ok(_) => {
-                if line.contains(&args.pattern) {
-                    println!("{}", line);
+                if line.contains(pattern) {
+                    writeln!(writer, "{}", line).expect("writer is broken!");
                 }
             },
             Err(_) => panic!("this is a terrible mistake!"),
         }
     }
+
+
+}
+
+fn main() {
+    let args = Cli::parse();
+    find_patern_in_file(&args.pattern, &args.file_path ,&mut std::io::stdout())
+}
+
+#[test]
+fn find_a_match() -> Result<(), Box<dyn std::error::Error>> {
+    let mut result = Vec::new();
+    
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    //let mut file = tempfile()?;
+
+    file.write_str("A test\nActual content\nMore content\nAnother test")?;
+    
+    find_patern_in_file(&String::from("test"), 
+                        file.path(),
+                        &mut result);
+    assert_eq!(result, b"A test\n\nAnother test\n");
+    Ok(())
 }
--- grrs/src/test.rs
+++
@@ -1,8 +0,0 @@
-fn answer() -> i32{
-    42
-}
-
-#[test]
-fn check_answer_validity() {
-    assert_eq!(answer(), 42);
-}
