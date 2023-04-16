c0c0e57dbc332d31779ece733689945be0173b4c
Aleksandar Milicevic <milicoa97@gmail.com>
2023-04-17 01:03:15 +0200

    add simple polars example to download csv

--- polars-utils/src/lib.rs
+++ polars-utils/src/lib.rs
@@ -1,4 +1,26 @@
+use std::path::Path;
+use color_eyre::{Result};
+use reqwest::blocking::Client;
+use polars::prelude::*;
+use std::io::Cursor;
 
+fn download_csv(url: &str, path_to_save: &Path) -> Result<()>{
+    let data: Vec<u8> = Client::new()
+        .get(url)
+        .send()?
+        .text()?
+        .bytes()
+        .collect();
+    let mut df = CsvReader::new(Cursor::new(data))
+        .has_header(true)
+        .finish()?;
+     
+    let mut file = std::fs::File::create(path_to_save).unwrap();
+    CsvWriter::new(&mut file).finish(& mut df).unwrap();
+    println!("{:?}", df.shape());
+    println!("{:?}",df);
+    Ok(())
+}
 
 #[cfg(test)]
 mod tests {
@@ -6,7 +28,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        download_csv("https://j.mp/iriscsv", Path::new("./iris.csv"));
     }
 }
