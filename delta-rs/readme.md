e22e21774c10ac587439ef0b32e9818ef4fa6fd2
Aleksandar Milicevic <milicoa97@gmail.com>
2023-04-19 02:06:24 +0200

    add basic delta table read/write; add vanila streaming

---
+++ delta-rs/readme.md
@@ -0,0 +1,6 @@
+## Source 
+Trying to get familiar with the delta lake format and delta-rs library 
+
+https://docs.delta.io/latest/concurrency-control.html
+https://github.com/delta-io/delta-rs
+
--- delta-rs/wiki_stream.py
+++ delta-rs/wiki_stream.py
@@ -5,10 +5,13 @@ if __name__ == "__main__":
     import pyspark
     from delta import *
 
-    builder = pyspark.sql.SparkSession.builder.appName("MyApp").config("spark.local.dir", "./tmp/spark-temp") \
+    builder = pyspark.sql.SparkSession.builder.appName("MyApp") \
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
 
     spark = configure_spark_with_delta_pip(builder).getOrCreate()
     data = spark.range(0, 5)
-    data.write.format("delta").save("./tmp/delta-table")
+    data.write.format("delta").mode("overwrite").save("./tmp/delta-table")
+    data = spark.range(5, 10)
+    data.show()
+    data.write.format("delta").mode("overwrite").save("./tmp/delta-table")
